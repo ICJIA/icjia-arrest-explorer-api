6de5f3b129a9a83dfbffee698fde3956c16a6d50
Bobae Kang <bkang.dev@gmail.com>
2020-01-14 10:33:27 -0600

    add cache to storage option

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import * as data from './data'
 import * as rest from './http/rest'
-import * as memory from './storage/memory'
+import * as cache from './storage/cache'
+// import * as memory from './storage/memory'
 
-const storage = memory.NewStorage()
-const dataService = data.NewService(storage)
-const app = rest.NewApp(dataService)
+const main = async (): Promise<void> => {
+  const storage = await cache.NewStorage()
+  // const storage = memory.NewStorage()
+  const dataService = data.NewService(storage)
+  const app = rest.NewApp(dataService)
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Listening on port ${port}.`)
-})
+  const port = 3000
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}.`)
+  })
+}
+
+main()
---
+++ src/storage/cache.ts
@@ -0,0 +1,42 @@
+import sqlite3 from 'sqlite3'
+import * as data from './../data'
+
+type Storage = {
+  getArrestsAll: () => data.Table
+  getArrestsByOffenseClass: () => data.Table
+}
+
+const fetchFromDB = async (
+  db: sqlite3.Database,
+  tableName: string,
+): Promise<data.Table> => {
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.all(`SELECT * FROM "${tableName}"`, (err: Error, rows) => {
+        if (err) reject(err)
+        else resolve(data.unflatten(rows as data.TableDTO))
+      })
+    })
+  })
+}
+
+const init = async (): Promise<data.Table[]> => {
+  const sqlite3 = require('sqlite3').verbose()
+  const db = new sqlite3.Database('../database.db')
+
+  const arrestsAll = await fetchFromDB(db, 'ArrestsAll')
+  const arrestsByOffenseClass = await fetchFromDB(db, 'ArrestsByOffenseClass')
+
+  db.close()
+
+  return [arrestsAll, arrestsByOffenseClass]
+}
+
+export const NewStorage = async (): Promise<Storage> => {
+  const [arrestsAll, arrestsByOffenseClass] = await init()
+
+  return {
+    getArrestsAll: (): data.Table => arrestsAll,
+    getArrestsByOffenseClass: (): data.Table => arrestsByOffenseClass,
+  }
+}
